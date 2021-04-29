acbb870577981bda4e4689e31e36c23d38d4e1fe
Ariel Armijo <arielarmijo@yahoo.es>
2021-04-29 12:09:26 -0400

    Arregla error CORS

--- portafolio/src/main/java/tk/monkeycode/portafolio/controller/PortafolioController.java
+++ portafolio/src/main/java/tk/monkeycode/portafolio/controller/PortafolioController.java
@@ -26,7 +26,7 @@ import tk.monkeycode.portafolio.service.StorageService;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"http://localhost:4200", "http://monkeycode.tk"})
 public class PortafolioController {
 	
 	@Autowired
